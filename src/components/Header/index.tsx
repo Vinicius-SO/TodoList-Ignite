3d68231f7fb454d4088e7d831d15dc11fc386440
Vinicius-SO <vhisoares39@gmail.com>
2022-08-11 08:57:20 -0300

    Component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,11 @@
+import rocketImg from '../../assets/rocket.svg'
+import todoImg from '../../assets/todo.svg'
+import styles from './Header.module.css'
 export function Header(){
     return(
-        <h2>Header</h2>
+        <header className={styles.header}>
+            <img src={rocketImg} alt="" />
+            <img src={todoImg} alt="" />       
+        </header>
     )
 }
