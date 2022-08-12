b986ff3fc41e3703f18d9286a9aea54461d0f2d2
Vinicius-SO <vhisoares39@gmail.com>
2022-08-12 18:08:37 -0300

    Task List Empty

--- src/components/TaskListEmpty/index.tsx
+++ src/components/TaskListEmpty/index.tsx
@@ -1,5 +1,15 @@
+import clipboard from '../../assets/Clipboard.svg'
+
+import styles from './TaskListEmpty.module.css'
+
 export function TaskListEmpty(){
     return(
-        <h1>EmptyTaskList</h1>
+        <div className={styles.wrapper}>
+            <div>
+                <img src={clipboard} alt="" />
+            </div>
+            <p>Você ainda não tem tarefas cadastradas</p>
+            Crie tarefas e organize seus itens a fazer
+        </div>
     )
-}
+}  
--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,9 +1,13 @@
+import { TaskListEmpty } from "../TaskListEmpty";
 import { TasksInfos } from "../TasksInfos";
 import styles from "./Tasks.module.css"
 export function Tasks(){
     return (
-        <div className={styles.wrapper}>
-            <TasksInfos/>
-        </div>
+        <>
+            <div className={styles.wrapper}>
+                <TasksInfos />
+                <TaskListEmpty/>
+            </div>
+        </>
     )
 }
