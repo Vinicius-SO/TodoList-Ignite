48edc0fa88a8f07d483ce6316ce37afc930d538f
Vinicius-SO <vhisoares39@gmail.com>
2022-08-15 00:51:17 -0300

    COmponent: TaskItem

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -5,9 +5,11 @@ import styles from './TaskItem.module.css'
 export function TaskItem(){
     return(
         <div className={styles.container}>
-            <input type="checkbox" />
-            <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
-            <Trash />
+            <input id=''type="checkbox" />
+            <label htmlFor=''>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.</label>
+            <button>
+                <Trash size={20}/>
+            </button>
         </div>
     )   
 }
