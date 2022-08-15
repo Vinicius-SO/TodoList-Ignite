c913ac5580b83df301721628bfbe4d3bd29b7d63
Vinicius-SO <vhisoares39@gmail.com>
2022-08-14 22:30:53 -0300

    Task item html

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,5 +1,13 @@
+import { Trash } from "phosphor-react";
+
+import styles from './TaskItem.module.css'
+
 export function TaskItem(){
     return(
-        <h1>TaskItem</h1>
+        <div className={styles.container}>
+            <input type="checkbox" />
+            <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+            <Trash />
+        </div>
     )   
 }
